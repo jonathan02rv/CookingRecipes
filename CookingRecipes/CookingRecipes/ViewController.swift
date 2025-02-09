1158fc24b2bc73b7d2e0b1da7d058bf2a226dc31
Jhonatahan Rivera <jonathan02rv@gmail.com>
2025-02-09 13:57:57 -0500

    clean project

--- CookingRecipes/CookingRecipes/ViewController.swift
+++ CookingRecipes/CookingRecipes/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by JHONATAHAN RIVERA on 2/9/25.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
