bdf5cec79c485540efe731b5e04583786755bc92
Jhonatahan Rivera <jonathan02rv@gmail.com>
2025-02-09 12:06:29 -0500

    create project

---
+++ CookingRecipes/CookingRecipes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CookingRecipes
+//
+//  Created by JHONATAHAN RIVERA on 2/9/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
